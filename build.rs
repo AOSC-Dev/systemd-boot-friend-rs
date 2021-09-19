1bf10268cf5788387eddd600313493a5d77cde04
OriginCode <self@origincode.me>
2021-09-19 18:05:14 +0800

    feat: generate shell completions

---
+++ build.rs
@@ -0,0 +1,41 @@
+use clap::IntoApp;
+use clap_generate::{
+    generate,
+    generators::{Bash, Fish, Zsh},
+    Generator,
+};
+use std::{env, fs, io::Result};
+
+include!("src/cli.rs");
+
+const ROOT: &str = "completions";
+const APP: &str = "systemd-boot-friend";
+
+macro_rules! generate_shell_completions {
+    ($app:ident, $shell:ident) => {
+        generate::<$shell, _>(
+            &mut $app,
+            APP,
+            &mut fs::File::create(format!("{}/{}", ROOT, $shell::file_name(APP)))?,
+        );
+    }
+}
+
+fn generate_completions() -> Result<()> {
+    fs::create_dir_all(ROOT)?;
+    let mut app = Opts::into_app();
+    generate_shell_completions!(app, Bash);
+    generate_shell_completions!(app, Fish);
+    generate_shell_completions!(app, Zsh);
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    println!("cargo:rerun-if-env-changed=SBF_GEN_COMPLETIONS");
+    if env::var("SBF_GEN_COMPLETIONS").is_ok() {
+        generate_completions()?;
+    }
+
+    Ok(())
+}
---
+++ completions/systemd-boot-friend.bash
@@ -0,0 +1,167 @@
+_systemd-boot-friend() {
+    local i cur prev opts cmds
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+    cmd=""
+    opts=""
+
+    for i in ${COMP_WORDS[@]}
+    do
+        case "${i}" in
+            systemd-boot-friend)
+                cmd="systemd__boot__friend"
+                ;;
+            
+            help)
+                cmd+="__help"
+                ;;
+            init)
+                cmd+="__init"
+                ;;
+            install)
+                cmd+="__install"
+                ;;
+            list)
+                cmd+="__list"
+                ;;
+            list-installed)
+                cmd+="__list__installed"
+                ;;
+            remove)
+                cmd+="__remove"
+                ;;
+            update)
+                cmd+="__update"
+                ;;
+            *)
+                ;;
+        esac
+    done
+
+    case "${cmd}" in
+        systemd__boot__friend)
+            opts=" -h -V  --help --version  init list install list-installed remove update help"
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        
+        systemd__boot__friend__help)
+            opts=" -h -V  --help --version  "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        systemd__boot__friend__init)
+            opts=" -h -V  --help --version  "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        systemd__boot__friend__install)
+            opts=" -f -h -V  --force --help --version  <TARGET> "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        systemd__boot__friend__list)
+            opts=" -h -V  --help --version  "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        systemd__boot__friend__list__installed)
+            opts=" -h -V  --help --version  "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        systemd__boot__friend__remove)
+            opts=" -h -V  --help --version  <TARGET> "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+        systemd__boot__friend__update)
+            opts=" -h -V  --help --version  "
+            if [[ ${cur} == -* || ${COMP_CWORD} -eq 2 ]] ; then
+                COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+                return 0
+            fi
+            case "${prev}" in
+                
+                *)
+                    COMPREPLY=()
+                    ;;
+            esac
+            COMPREPLY=( $(compgen -W "${opts}" -- "${cur}") )
+            return 0
+            ;;
+    esac
+}
+
+complete -F _systemd-boot-friend -o bashdefault -o default systemd-boot-friend
