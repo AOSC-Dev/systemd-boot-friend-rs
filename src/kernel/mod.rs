38a5b4bc974f9d5f2e7825afed40386d268c6bd0
Kaiyang Wu <origincode@aosc.io>
2022-04-22 23:51:32 -0700

    kernel: use `same_file::is_same_file()` to identify the same files

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
-use sha2::{Digest, Sha256};
-use std::{fmt::Display, fs, io, path::Path};
+use same_file::is_same_file;
+use std::{fmt::Display, fs, path::Path};
 
 const REL_ENTRY_PATH: &str = "loader/entries/";
 
@@ -14,22 +14,9 @@ pub trait Kernel: Display + Clone + PartialEq {
     fn install_and_make_config(&self, force_write: bool) -> Result<()>;
 }
 
-// Check if two files are identical
-fn same_files<P: AsRef<Path>>(file1: P, file2: P) -> Result<bool> {
-    let mut hasher1 = Sha256::new();
-    io::copy(&mut fs::File::open(&file1)?, &mut hasher1)?;
-    let hash1 = hasher1.finalize();
-
-    let mut hasher2 = Sha256::new();
-    io::copy(&mut fs::File::open(&file2)?, &mut hasher2)?;
-    let hash2 = hasher2.finalize();
-
-    Ok(hash1 == hash2)
-}
-
 pub fn file_copy<P: AsRef<Path>>(src: P, dest: P) -> Result<()> {
     // Only copy if the dest file is missing / different
-    if !dest.as_ref().exists() || !same_files(&src, &dest)? {
+    if !dest.as_ref().exists() || !is_same_file(&src, &dest)? {
         fs::copy(&src, &dest)?;
     }
 
