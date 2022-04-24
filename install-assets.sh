e28a343edfba36809b804f832dcc802b6ffcbbc0
Kaiyang Wu <origincode@aosc.io>
2022-04-24 02:49:31 -0700

    fix: remove old completion files, use new ones in `install_assets.sh`

--- install-assets.sh
+++ install-assets.sh
@@ -4,8 +4,8 @@ PREFIX="${PREFIX:-/usr/local}"
 
 # install completions
 install -dv "${PREFIX}/share/zsh/functions/Completion/Linux/"
-install -Dvm644 completions/_systemd-boot-friend "${PREFIX}/share/zsh/functions/Completion/Linux/"
+install -Dvm644 completions/_sbf "${PREFIX}/share/zsh/functions/Completion/Linux/"
 install -dv "${PREFIX}/share/fish/completions/"
-install -Dvm644 completions/systemd-boot-friend.fish "${PREFIX}/share/fish/completions/"
+install -Dvm644 completions/sbf.fish "${PREFIX}/share/fish/completions/"
 install -dv "${PREFIX}/share/bash-completion/completions/"
-install -Dvm644 completions/systemd-boot-friend.bash "${PREFIX}/share/bash-completion/completions/"
+install -Dvm644 completions/sbf.bash "${PREFIX}/share/bash-completion/completions/"
